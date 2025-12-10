555aa7effa0f6b0cfa7c2789a0b107ec13c3ba3e
Hossein Maktoobian <atikalaker@gmail.com>
2025-12-10 14:41:13 +0330

    add images

--- images.js
+++ images.js
@@ -1,15 +1,9 @@
 // Image data for Voorivex Art Gallery
 const images = [
-	{ src: 'https://picsum.photos/400/600?random=1', alt: 'Abstract Digital Art #001', size: 'large' },
-	{ src: 'https://picsum.photos/400/300?random=2', alt: 'Cyberpunk Landscape #002', size: 'medium' },
-	{ src: 'https://picsum.photos/400/500?random=3', alt: 'Neural Network Visualization #003', size: 'medium' },
-	{ src: 'https://picsum.photos/400/400?random=4', alt: 'Quantum Computing Art #004', size: 'small' },
-	{ src: 'https://picsum.photos/400/700?random=5', alt: 'Digital Matrix #005', size: 'large' },
-	{ src: 'https://picsum.photos/400/350?random=6', alt: 'Code Poetry #006', size: 'small' },
-	{ src: 'https://picsum.photos/400/450?random=7', alt: 'Binary Dreams #007', size: 'medium' },
-	{ src: 'https://picsum.photos/400/550?random=8', alt: 'Algorithmic Beauty #008', size: 'medium' },
-	{ src: 'https://picsum.photos/400/320?random=9', alt: 'Data Visualization #009', size: 'small' },
-	{ src: 'https://picsum.photos/400/480?random=10', alt: 'Virtual Reality Art #010', size: 'medium' },
-	{ src: 'https://picsum.photos/400/380?random=11', alt: 'Machine Learning Vision #011', size: 'small' },
-	{ src: 'https://picsum.photos/400/520?random=12', alt: 'Digital Consciousness #012', size: 'large' }
+	{ src: './images/sadra-saman.jpg', alt: 'Hacking Legends' },
+	{ src: './images/omid-ghelyoun.jpg', alt: 'Hunting with Ghelyoun' },
+	{ src: './images/aisan-bita.jpg', alt: 'Hacking Legends - Remastered' },
+	{ src: './images/yashar-sleeping.jpg', alt: 'Yashar is Sleeping...' },
+	{ src: './images/hackers-childhood.jpg', alt: 'Sadra being a Child' },
+	{ src: './images/reading-along-name.jpg', alt: 'Reading a long name is hard. Isn\'t it?' },
 ];
